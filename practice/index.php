563c3bf0f0e3639441b71dcaadf29549c189dea0
Keryn Egan <keryn.egan@gmail.com>
2021-02-08 23:16:36 -0500

    initial commit

---
+++ practice/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang='en'>
+<head>
+    <title>Practice Application</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+
+<body>
+    <h1>Practice Application</h1>
+    <h2>Keryn Egan</h2>
+
+    <?php $x = 9 + 10; ?>
+    <?php echo 9 + 10; ?>
+</body>
+</html>
